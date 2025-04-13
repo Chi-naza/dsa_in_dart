675416f91eac11e564adc5510936a6ca09d1f381
Chi-naza <ugwuokechinazajames@gmail.com>
2025-04-13 23:20:34 +0100

    Started working on and studying graphs

---
+++ lib/graphs/graphs.dart
@@ -0,0 +1,20 @@
+void main() {
+  //
+}
+
+
+
+/* 
+    Graphs
+      A graph is a data structure that captures relationships between objects. 
+      It’s made up of vertices connected by edges.
+      Graphs are an instrumental data structure that can model a wide range of things: 
+       a. webpages on the internet, 
+       b. the migration patterns of birds, 
+       c. even protons in the nucleus of an atom
+
+    Types of Graph
+      - Weighted Graphs: In a weighted graph, every edge has a weight associated with it that represents the cost of using this edge.
+      - Directed Graphs: This is a graph where the egdes have direction, and this can be restrictive because an edge may only permit traversal in one direction.
+      - Undirected Graphs: It is a directed-graph where all the edges are bi-drectional.
+*/
